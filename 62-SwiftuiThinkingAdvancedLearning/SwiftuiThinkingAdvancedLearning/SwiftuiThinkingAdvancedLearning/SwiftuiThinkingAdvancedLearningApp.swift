c9812b1f353e3e5b5641e10591ad6ef902fe5e52
张芳涛 <>
2023-07-05 00:39:18 +0800

    Advanced

---
+++ 62-SwiftuiThinkingAdvancedLearning/SwiftuiThinkingAdvancedLearning/SwiftuiThinkingAdvancedLearning/SwiftuiThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiThinkingAdvancedLearningApp.swift
+//  SwiftuiThinkingAdvancedLearning
+//
+//  Created by 张芳涛 on 2023/7/4.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ViewModifierBootcamp()
+        }
+    }
+}
---
+++ 62-ViewModifierBootcamp/ViewModifierBootcamp/ViewModifierBootcamp/ViewModifierBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifierBootcampApp.swift
+//  ViewModifierBootcamp
+//
+//  Created by 张芳涛 on 2023/7/5.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifierBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
