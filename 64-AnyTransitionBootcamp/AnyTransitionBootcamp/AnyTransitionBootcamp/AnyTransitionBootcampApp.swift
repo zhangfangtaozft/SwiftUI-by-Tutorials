d405b2b1faeadd7860ccf907a1e458c73ad714ce
张芳涛 <>
2023-07-05 03:18:20 +0800

    AnyTransitionBootcamp

---
+++ 64-AnyTransitionBootcamp/AnyTransitionBootcamp/AnyTransitionBootcamp/AnyTransitionBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyTransitionBootcampApp.swift
+//  AnyTransitionBootcamp
+//
+//  Created by 张芳涛 on 2023/7/5.
+//
+
+import SwiftUI
+
+@main
+struct AnyTransitionBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
