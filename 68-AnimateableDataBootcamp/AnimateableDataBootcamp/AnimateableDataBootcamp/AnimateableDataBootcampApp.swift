7e6a329d0f5c6c21e5241e5098ab171778036e4c
张芳涛 <>
2023-07-06 03:53:32 +0800

    AnimateableDataBootcamp

---
+++ 68-AnimateableDataBootcamp/AnimateableDataBootcamp/AnimateableDataBootcamp/AnimateableDataBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimateableDataBootcampApp.swift
+//  AnimateableDataBootcamp
+//
+//  Created by 张芳涛 on 2023/7/6.
+//
+
+import SwiftUI
+
+@main
+struct AnimateableDataBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
