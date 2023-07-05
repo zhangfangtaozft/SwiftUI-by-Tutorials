b4b7eadb9a2c10348f114ac8f573933d39bac26a
张芳涛 <>
2023-07-05 18:30:14 +0800

    add

---
+++ 66-CustShapeBootcamp/CustShapeBootcamp/CustShapeBootcamp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CustShapeBootcamp
+//
+//  Created by 张芳涛 on 2023/7/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 66-CustShapeBootcamp/CustShapeBootcamp/CustShapeBootcamp/CustShapeBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustShapeBootcampApp.swift
+//  CustShapeBootcamp
+//
+//  Created by 张芳涛 on 2023/7/5.
+//
+
+import SwiftUI
+
+@main
+struct CustShapeBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
