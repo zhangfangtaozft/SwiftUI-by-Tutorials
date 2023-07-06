ea9db86746cc9a47319a45a2a0ea9a4aad2a56f5
Frank <894918941@qq.com>
2023-07-07 01:25:49 +0800

    GenericsBootcamp

---
+++ 67-GenericsBootcamp/GenericsBootcamp/GenericsBootcamp/GenericsBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericsBootcampApp.swift
+//  GenericsBootcamp
+//
+//  Created by 张芳涛 on 2023/7/6.
+//
+
+import SwiftUI
+
+@main
+struct GenericsBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
